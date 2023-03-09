02f5d635b6797db04e2105a65fbe06c2ef6cfe04
kennavaughn <125404329+kennavaughn@users.noreply.github.com>
2023-03-09 15:23:04 -0600

    Update scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,21 +17,81 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
+    if(inputValue === ''){
+        return 'Empty';
+    } else if (isNaN(value)){
+        return 'Not a Number';
+    } else {
+        return 'Is a Number';
+    }
    
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    const validationResults = {
+        pilot: validateInput(pilot),
+        copilot: validateInput(copilot),
+        fuelLevel: validateInput(fuelLevel),
+        cargoLevel: validateInput(cargoLevel)
+    };
+    if (
+        validationResults.pilot === 'Empty' ||
+        validationResults.copilot === 'Empty' ||
+        validationResults.fuelLevel === 'Empty' ||
+        validationResults.cargoLevel === 'Empty' ||
+    ){
+        document.querySelector('#launch-status').classList.remove('ready');
+        document.querySelector('#launch-status').classList.add('not-ready');
+        document.querySelector('#shuttle-status').innerHTML = ('Awaiting');
+    }
 }
 
+// function to fetch planetary JSON
 async function myFetch() {
-    let planetsReturned;
+    const url = "https://handlers.education.launchcode.org/static/planets.json";
+    try {
+      return await response.json();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  // function to pick a random planet from a list
+  function pickPlanet(planets) {
+    const index = Math.floor(Math.random() * planets.length);
+    return planets[index];
+  }
+  
+  // function to update mission target div with planet data
+  function addDestinationInfo(document, planet) {
+    const missionTarget = document.getElementById("missionTarget");
+    missionTarget.innerHTML = `
+      <h2>Mission Destination</h2>
+      <ol>
+        <li>Name: ${planet.name}</li>
+        <li>Diameter: ${planet.diameter}</li>
+        <li>Star: ${planet.star}</li>
+        <li>Distance from Earth: ${planet.distance}</li>
+        <li>Number of Moons: ${planet.moons}</li>
+      </ol>
+      <img src="${planet.image}">
+    `;
+  }
+  let listedPlanets;
+// Set listedPlanetsResponse equal to the value returned by calling myFetch()
+let listedPlanetsResponse = myFetch();
 
-    planetsReturned = await fetch().then( function(response) {
-        });
-
-    return planetsReturned;
-}
+listedPlanetsResponse
+  .then(function (result) {
+    listedPlanets = result;
+    console.log(listedPlanets);
+  })
+  .then(function () {
+    console.log(listedPlanets);
+    // Call the appropriate helper functions to pick a planet from the list of planets and add that information to your destination
+    let planet = pickPlanet(listedPlanets);
+    addDestinationInfo(planet);
+  });
 
 function pickPlanet(planets) {
 }
